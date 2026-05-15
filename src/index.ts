218661ad82db4ac53026c62349c772d6cd360fe9
MontyG33 <lamontmauricegordon@gmail.com>
2026-05-15 13:21:43 -0700

    Refactor R2Explorer to support fetch with routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-import { R2Explorer } from "r2-explorer";
+interface Env {
+  BUCKET: R2Bucket;
+}
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: true,
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    };
 
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
-});
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { headers });
+    }
+
+    // Your existing routes...
+
+    if (request.method === 'GET' && url.pathname === '/api/files') {
+      const listed = await env.BUCKET.list();
+      const files = listed.objects.map(obj => ({
+        key: obj.key,
+        size: obj.size,
+        uploaded: obj.uploaded,
+        type: obj.httpMetadata?.contentType || 'unknown',
+      }));
+      return new Response(JSON.stringify({ files }), { headers });
+    }
+
+    // ... (add the rest of your routes)
+
+    return new Response('Not Found', { status: 404, headers });
+  },
+};
