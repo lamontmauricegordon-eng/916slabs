d341b9b81ae3f8bad660bfbf1cd8aaf5b0a746e2
MontyG33 <lamontmauricegordon@gmail.com>
2026-05-19 09:51:30 -0700

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
+/* eslint-disable @typescript-eslint/no-explicit-any -- Reason: Temporary any for Cloudflare env types */
 type Fetcher = {
   fetch(
     request: RequestInfo | Request,
@@ -9,7 +8,7 @@ type Fetcher = {
 
 type KVNamespace = any;
 type DurableObject = any;
-type R2Bucket = any;
+type R2Bucket = 916-slabs;
 type R2Object = any;
 
 declare global {
@@ -20,9 +19,10 @@ declare global {
   };
 }
 
-// Default fetch handler for Cloudflare Workers
+// Default fetch handler for Cloudflare Worker 
 export default {
-  async fetch(request: Request, env: { [key: string]: any }): Promise<Response> {
-    return new Response(`Hello from ${ENVIRONMENT}, version ${VERSION_METADATA.id}`);
+  async fetch(request: Request, env: any): Promise<Response> {
+    const secretValue = env.Hung_slabs;
+    return new Response(`Secret is set: ${env.Hung_slabs? 'Yes' : 'No'}`);
   }
 };
