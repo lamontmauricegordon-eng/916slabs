b0a6042c131bae4305b0f9caaf949c27a637c662
MontyG33 <lamontmauricegordon@gmail.com>
2026-05-19 06:21:22 -0700

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -22,7 +22,7 @@ declare global {
 
 // Default fetch handler for Cloudflare Workers
 export default {
-  fetch(request: Request, env: { [key: string]: any }): Promise<Response> {
+  async fetch(request: Request, env: { [key: string]: any }): Promise<Response> {
     return new Response(`Hello from ${ENVIRONMENT}, version ${VERSION_METADATA.id}`);
   }
 };
